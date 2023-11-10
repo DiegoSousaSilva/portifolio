3292b254bf877d1307f088bda888308a6327aa1f
Diego Sousa <derickbenji2@gmail.com>
2023-11-10 00:04:38 -0300

    component header | adding logic toogle theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,78 @@
+'use client'
+import { useState } from 'react'
+import Header from '../components/Header'
+
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
   return (
     <main>
+      <Header setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode} />
       <section
         id="home"
-        className="bg-darkBackground-500 flex h-screen flex-col items-center justify-center space-y-4"
+        className={`flex h-screen flex-col items-center justify-center space-y-4 ${
+          isDarkMode ? 'bg-darkBackground-500' : 'bg-lightBackground-500'
+        }`}
       >
-        <h1 className="text-textLight-500 text-5xl font-bold">
+        <h1
+          className={`text-5xl font-bold ${
+            isDarkMode ? 'text-textLight-500' : 'text-textDark-500'
+          }`}
+        >
           Desenvolvedor de Software
         </h1>
-        <h1 className="font-pressstart text-lightTitle-500 text-5xl">
+        <h1 className="font-pressstart text-5xl text-lightTitle-500">
           Diego Sousa
         </h1>
-        <p className="text-lightDescription-500 w-[576px] text-center text-2xl">
+        <p
+          className={`w-[576px] text-center text-2xl ${
+            isDarkMode
+              ? 'text-lightDescription-500'
+              : 'text-darkDescription-500'
+          }`}
+        >
           Freelancer que presta serviços com programação e desenvolvimento web.
           Precisa ter presença online? Junte-se a mim abaixo e vamos tirar a sua
           ideia do papel!
         </p>
       </section>
-      <section id="about-me" className="bg-lightDescription-500">
+      <section
+        id="about"
+        className={`${
+          isDarkMode ? 'bg-darkBackground-100' : 'bg-darkBackground-500'
+        }`}
+      >
         <div className="mx-auto flex w-4/5 flex-row py-6">
           <div>
-            <h4 className="text-textDark-500">
+            <h4
+              className={`${
+                isDarkMode
+                  ? 'text-darkDescription-900'
+                  : 'text-lightDescription-500'
+              }`}
+            >
               Seja bem-vindo ao meu portifólio
             </h4>
-            <h1 className="text-textDark-500 py-4 text-3xl font-bold">
-              Olá, meu nome é <span className="text-lightTitle-700">Diego</span>
+            <h1
+              className={`py-4 text-3xl font-bold ${
+                isDarkMode ? 'text-textDark-500' : 'text-textLight-500'
+              }`}
+            >
+              Olá, meu nome é{' '}
+              <span
+                className={`${
+                  isDarkMode ? 'text-lightTitle-900' : 'text-lightTitle-500'
+                }`}
+              >
+                Diego
+              </span>
               , prazer em conhecê-lo.
             </h1>
-            <p className="text-darkDescription-800 w-[576px] pt-3 text-2xl">
+            <p
+              className={`w-[576px] pt-3 text-2xl ${
+                isDarkMode ? 'text-textDark-500' : 'text-lightDescription-500'
+              }`}
+            >
               Apenas um apaixonado desenvolvedor de software e entusiasta da
               tecnologia, dedicado a criar soluções inovadoras que fazem a
               diferença. Com uma mente curiosa e uma paixão pela resolução de
@@ -37,7 +82,7 @@ export default function Home() {
             </p>
           </div>
           <div className="flex w-full items-center justify-center">
-            <p className="text-darkDescription-800  text-2xl">
+            <p className="text-2xl  text-darkDescription-800">
               [Aqui entra uma Imagem]
             </p>
           </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,140 @@
+import Link from 'next/link'
+import { Dispatch, SetStateAction, useState } from 'react'
+
+interface HeaderProps {
+  setIsDarkMode: Dispatch<SetStateAction<boolean>>
+  isDarkMode: boolean
+}
+
+const menuItems = [
+  {
+    href: '/',
+    text: 'Inicio',
+  },
+  {
+    href: '/#about',
+    text: 'Sobre',
+  },
+  {
+    href: '/#projects',
+    text: 'Portifólio',
+  },
+  {
+    href: '/#contact',
+    text: 'Contato',
+  },
+]
+
+const Header = ({ setIsDarkMode, isDarkMode }: HeaderProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
+  return (
+    <header
+      className={`flex items-center justify-between ${
+        isDarkMode
+          ? 'bg-darkBackground-500 text-textLight-500'
+          : 'bg-lightBackground-500 text-textDark-500'
+      } p-4 px-12 py-10`}
+    >
+      <div className="flex items-center">
+        <p className="font-pressstart text-sm font-bold">Diego Sousa</p>
+      </div>
+
+      {/* Ícone do menu para dispositivos móveis */}
+      <div className="lg:hidden">
+        <button onClick={toggleMenu}>
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* Ícone do menu */}
+            {isMenuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+        </button>
+      </div>
+
+      {/* Menu de navegação */}
+      <nav
+        className={`lg:flex lg:items-center ${isMenuOpen ? 'flex' : 'hidden'}`}
+      >
+        {menuItems.map((item) => {
+          return (
+            <Link
+              key={item.text}
+              href={item.href}
+              className="mr-4 mt-4 block text-xl lg:mt-0 lg:inline-block"
+            >
+              {item.text}
+            </Link>
+          )
+        })}
+      </nav>
+
+      {/* Botão para alternar o tema */}
+      <div className="flex items-center">
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? (
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {/* Ícone da lua (modo escuro) */}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 2L2 13M13 22l7-7-6-6 9-9M5 11a6 6 0 0010 0"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {/* Ícone do sol (modo claro) */}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 5a1 1 0 011 1v2a1 1 0 11-2 0V6a1 1 0 011-1zm0 12a1 1 0 100-2 1 1 0 000 2zm-9-6a1 1 0 011-1h2a1 1 0 010 2H4a1 1 0 01-1-1zm14 0a1 1 0 011-1h2a1 1 0 010 2h-2a1 1 0 01-1-1zM5 6a1 1 0 100 2 1 1 0 000-2z"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
